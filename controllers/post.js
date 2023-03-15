26a3f2a96ce95337825d89dde781840f1358f058
svk1102 <77896397+svk1102@users.noreply.github.com>
2023-03-16 02:29:24 +0530

    Initial commit

---
+++ controllers/post.js
@@ -0,0 +1,78 @@
+import mongoose from "mongoose"
+import PostMessage from "../models/postMessage.js"
+
+export const getPosts = async (req,res) => {
+    try {
+        const postMessages = await PostMessage.find()
+        // console.log(postMessages);
+        res.status(200).json(postMessages)
+    } catch (error) {
+        res.status(404).json({message : error.message})
+    }
+}
+
+export const createPost = async (req,res) => {
+    const post = req.body
+    const newPost = new PostMessage({...post,creator:req.userId, createdAt:new Date().toISOString()})
+
+    try {
+        await newPost.save();
+        res.status(201).json(newPost)
+
+    } catch (error) {
+        res.status(409).json({message:error.message})
+    }
+}
+
+export const updatePost = async (req,res) => {
+    const {id : _id} = req.params
+    const post = req.body
+    if(!mongoose.Types.ObjectId.isValid(_id)) {
+        return res.status(404).send("No post with that id");
+    }
+    const getPost = await PostMessage.findById(_id);
+    if(getPost.creator !== req.userId){
+        return res.status(404).send("Not the creator");
+    }
+
+    const updatedPost = await PostMessage.findByIdAndUpdate(_id,{...post,_id},{new : true})
+    res.json(updatedPost)
+
+}
+
+export const deletePost = async(req,res) => {
+    const {id : _id} = req.params
+    if(!mongoose.Types.ObjectId.isValid(_id)) {
+        return res.status(404).send("No post with that id");
+    }
+    const post = await PostMessage.findById(_id);
+    if(post.creator !== req.userId){
+        return res.status(404).send("Not the creator !");
+    }
+    
+    await PostMessage.findOneAndRemove(_id);
+    res.json({message : "Post Deleted Successfully"})
+}
+
+export const likePost = async(req,res) => {
+    const {id} = req.params
+
+    if(!req.userId) return res.json({message:"Not authenticated"})
+
+    if(!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).send("No post with that id");
+    }
+    const post = await PostMessage.findById(id);
+
+    const index = post?.likes.findIndex((id) => id === String(req.userId))
+
+    if(index === -1){
+        post.likes.push(req.userId)
+    }
+    else{
+        post.likes = post.likes.filter((id) => id !== String(req.userId))
+    }
+
+    const updatedPost = await PostMessage.findByIdAndUpdate(id,post,{new:true})
+    res.json(updatedPost) 
+}
---
+++ routes/posts.js
@@ -0,0 +1,13 @@
+import express from "express";
+import { getPosts , createPost , updatePost , deletePost , likePost} from "../controllers/post.js";
+import auth from "../middleware/auth.js";
+
+const postRoutes = express.Router();
+
+postRoutes.get("/",getPosts)
+postRoutes.post("/",auth ,createPost)
+postRoutes.patch("/:id",auth ,updatePost)
+postRoutes.delete("/:id",auth ,deletePost)
+postRoutes.patch("/:id/likePost",auth,likePost)
+
+export default postRoutes;
